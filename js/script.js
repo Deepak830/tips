75b44895fbd1ac230fd0db9cbd854fee4d25c860
Deepak830 <111578029+Deepak830@users.noreply.github.com>
2023-06-11 17:01:42 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*typing animation*/
+
+var typed = new Typed(".typing",{
+    strings:["", "web Designer","web Developer","Graphic Designer","Freelancer"],
+    typedSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
+
+
